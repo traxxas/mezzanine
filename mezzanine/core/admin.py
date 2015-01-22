b405029abc4fdb6e979731d4d1213b5d6ae7dde9
Alex Hill <alex@hill.net.au>
2015-01-22 09:17:59 +0800

    Deleted mezzanine/utils/deprecation.py
    This contained compatibility fixes for Django < 1.6, which we no longer
    support.
    
    Conflicts:
    	mezzanine/utils/views.py

--- mezzanine/core/admin.py
+++ mezzanine/core/admin.py
@@ -211,7 +211,6 @@ class StackedDynamicInlineAdmin(BaseDynamicInlineAdmin,
         super(StackedDynamicInlineAdmin, self).__init__(*args, **kwargs)
 
 
-@renamed_get_queryset
 class OwnableAdmin(admin.ModelAdmin):
     """
     Admin class for models that subclass the abstract ``Ownable``
